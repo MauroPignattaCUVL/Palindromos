4a779651022b05aa88815059659b26d7bdba17a6
MauroPignattaCUVL <54460406+MauroPignattaCUVL@users.noreply.github.com>
2019-09-03 00:20:24 -0300

    Update palindromo.c

--- src/palindromo.c
+++ src/palindromo.c
@@ -1,8 +1,8 @@
 #include "../includes/palindromo.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#include <stdbool.h>  //bool
+#include <string.h>  //memset
 
 #define LONG_TEXT 256
 
